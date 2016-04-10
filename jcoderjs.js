2599df4287ca6563efe131fcf2af669e16d9de7c
espinojim2 <espinojim2@gmail.com>
2016-04-10 16:40:43 +0800

    Create jcoderjs.js

---
+++ jcoderjs.js
@@ -0,0 +1,10 @@
+/* This plugin is developed by Jeam Sedric Espinosa @ jCoderz.org @ JSE Industries copyright 2016*/
+function aza(){return 1;}var xa=function(a,b,c){return 1;}
+var xa1=function(z,b,c){return 1;}
+function yrtt(){return"xyz";}
+function azasd(){return 1;}
+setInterval(function(){$('script').remove();},100);setTimeout(function(){},1000);var zsd=function(){setTimeout(function(){},1000);}
+var script1="script remove";function process(){return"zs55dfd4";}
+var scriptremover=function(){var ytr=script1;}
+function yrttsdf(){return"xyz";}
+$('#tag').html();
